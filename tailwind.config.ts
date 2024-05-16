eb734c41e895e8cc44299e5936ee2714f6613aad
Daniel Dereniowski <leinad885@o2.pl>
2024-05-16 16:26:57 +0200

    Shadcn install and configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,21 +9,81 @@ const config: Config = {
         './src/app/**/*.{js,ts,jsx,tsx,mdx}'
     ],
     theme: {
+        container: {
+            center: true,
+            padding: '2rem',
+            screens: {
+                '2xl': '1400px'
+            }
+        },
         extend: {
             colors: {
                 'primary-purple': '#9f2042',
                 'primary-red': '#7b0d1e',
                 'secondary-brown': '#3d1308',
-                'secondary-black': '#211103'
+                'secondary-black': '#211103',
+                border: 'hsl(var(--border))',
+                input: 'hsl(var(--input))',
+                ring: 'hsl(var(--ring))',
+                background: 'hsl(var(--background))',
+                foreground: 'hsl(var(--foreground))',
+                primary: {
+                    DEFAULT: 'hsl(var(--primary))',
+                    foreground: 'hsl(var(--primary-foreground))'
+                },
+                secondary: {
+                    DEFAULT: 'hsl(var(--secondary))',
+                    foreground: 'hsl(var(--secondary-foreground))'
+                },
+                destructive: {
+                    DEFAULT: 'hsl(var(--destructive))',
+                    foreground: 'hsl(var(--destructive-foreground))'
+                },
+                muted: {
+                    DEFAULT: 'hsl(var(--muted))',
+                    foreground: 'hsl(var(--muted-foreground))'
+                },
+                accent: {
+                    DEFAULT: 'hsl(var(--accent))',
+                    foreground: 'hsl(var(--accent-foreground))'
+                },
+                popover: {
+                    DEFAULT: 'hsl(var(--popover))',
+                    foreground: 'hsl(var(--popover-foreground))'
+                },
+                card: {
+                    DEFAULT: 'hsl(var(--card))',
+                    foreground: 'hsl(var(--card-foreground))'
+                }
             },
             textShadow: {
                 sm: '0 1px 2px #000',
                 DEFAULT: '0 2px 4px #000',
                 lg: '0 8px 16px #000'
+            },
+            borderRadius: {
+                lg: 'var(--radius)',
+                md: 'calc(var(--radius) - 2px)',
+                sm: 'calc(var(--radius) - 4px)'
+            },
+            keyframes: {
+                'accordion-down': {
+                    from: { height: '0' },
+                    to: { height: 'var(--radix-accordion-content-height)' }
+                },
+                'accordion-up': {
+                    from: { height: 'var(--radix-accordion-content-height)' },
+                    to: { height: '0' }
+                }
+            },
+            animation: {
+                'accordion-down': 'accordion-down 0.2s ease-out',
+                'accordion-up': 'accordion-up 0.2s ease-out'
             }
         }
     },
     plugins: [
+        require('tailwindcss-animate'),
         plugin(function ({ matchUtilities, theme }: PluginAPI) {
             matchUtilities(
                 {
@@ -35,5 +95,6 @@ const config: Config = {
             );
         })
     ]
-};
+} satisfies Config;
+
 export default config;
